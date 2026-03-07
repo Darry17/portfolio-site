df14c2cececc655f695ad35e30c926b762a69ddf
Darry Diaz <d.diaz+github@psmnd.dev>
2026-03-07 12:45:11 +0800

    added micro interactions

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,8 @@
-import { Component, HostListener, signal, AfterViewInit } from '@angular/core';
+import { Component, HostListener, signal, AfterViewInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+const SCROLL_STOP_DELAY_MS = 200;
+const TOP_THRESHOLD_PX = 20;
 
 @Component({
   selector: 'app-header',
@@ -8,16 +11,44 @@ import { CommonModule } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements AfterViewInit {
+export class HeaderComponent implements AfterViewInit, OnDestroy {
   activeSection = signal('');
+  headerVisible = signal(true);
+
+  private scrollStopTimer: ReturnType<typeof setTimeout> | null = null;
 
   ngAfterViewInit(): void {
     this.updateActiveSection();
   }
 
+  ngOnDestroy(): void {
+    if (this.scrollStopTimer != null) {
+      clearTimeout(this.scrollStopTimer);
+    }
+  }
+
   @HostListener('window:scroll', [])
   onWindowScroll(): void {
+    const scrollY = typeof window !== 'undefined' ? window.scrollY : 0;
     this.updateActiveSection();
+
+    if (scrollY <= TOP_THRESHOLD_PX) {
+      this.headerVisible.set(true);
+      if (this.scrollStopTimer != null) {
+        clearTimeout(this.scrollStopTimer);
+        this.scrollStopTimer = null;
+      }
+      return;
+    }
+
+    this.headerVisible.set(false);
+    if (this.scrollStopTimer != null) {
+      clearTimeout(this.scrollStopTimer);
+    }
+    this.scrollStopTimer = setTimeout(() => {
+      this.headerVisible.set(true);
+      this.scrollStopTimer = null;
+    }, SCROLL_STOP_DELAY_MS);
   }
 
   private updateActiveSection(): void {
